b3a014c8c49aa89911726255b637905e3a6d1569
Subhrajyoti <subha.ionic3@gmail.com>
2020-12-21 12:28:19 +0530

    Initial Commit

---
+++ FanMeals/FanMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanMealsApp.swift
+//  FanMeals
+//
+//  Created by Subhrajyoti Chakraborty on 21/12/20.
+//
+
+import SwiftUI
+
+@main
+struct FanMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
